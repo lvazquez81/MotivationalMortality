286a4f411be376422cb86e60a5ad17de5c778c9d
lvazquez81 <lvazquez81@outlook.com>
2015-07-17 22:54:40 -0700

    Adding web project

---
+++ MotivationalMortailityWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MotivationalMortailityWeb.MvcApplication" Language="C#" %>
---
+++ MotivationalMortailityWeb/Models/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MotivationalMortailityWeb.Models
+{
+    public class MainViewModel
+    {
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+        
+        public DateTime Today { get; set; }
+        public int NumberOfWeeks { get; set; }
+    }
+}
